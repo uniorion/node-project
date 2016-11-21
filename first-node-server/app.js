d0c78585f92f41354683b97abedfde2a5ce1c9cf
uniorion <uniorion@naver.com>
2016-11-21 16:30:17 +0900

    add home route:
    add home route in app.js
    print request.url on console

--- first-node-server/app.js
+++ first-node-server/app.js
@@ -1,10 +1,17 @@
 var http = require('http');
 
 
-http.createServer(function(request, response){
-	response.writeHead(200, {'Content-Type':'text/plain'});
-	response.write("Hello NodeJS World~~");
-	response.end();	
+var app = http.createServer(function(request, response){
+	// response.writeHead(200, {'Content-Type':'text/plain'});
+	// response.write("Hello NodeJS World~~");
+	// response.end();
+  console.log(request.url);
+
+  if ( request.url === "/" ) {
+    // root일 경우에만 출력
+    response.write("This is HOME!!!!!!!!");
+  }
+  response.end();
 }).listen(process.env.PORT || 3030);
 
 console.log("First Node Server is running at localhost");
