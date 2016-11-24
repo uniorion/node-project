6135d5789ae7cbd6d8b921766ab70c4770d08091
uniorion <uniorion@naver.com>
2016-11-24 16:50:54 +0900

    web page scraping with request, cheerio

--- tomatoexpress/server.js
+++ tomatoexpress/server.js
@@ -1,12 +1,41 @@
 var express = require('express');
 var app = express();
 
+var request = require('request');
+var cheerio = require('cheerio');
+
+var boxOffice = [];
 
 app.set('port', process.env.PORT || 3030); 
 
-app.get('/', function(req, res){
-  res.send('Hello Tomato~');
+app.get('/', function(req, res) {
+  request({
+    method: 'GET', 
+    uri: 'https://www.rottentomatoes.com/'
+  }, function(err, res, body){
+      if (err) {
+        throw err;
+      }
+      var $ = cheerio.load(body);
+
+      var movieinfo = $('table#Top-Box-Office.movie_list');
+      
+      movieinfo.each(function() {
+        var movieName   = $(this).find('td.middle_col').text();
+        var meterScore  = $(this).find('td.left_col').text();
+
+        console.log(movieName);
+        console.log(meterScore);
+
+        boxOffice.push({movieName: movieName, meterScore: meterScore});
+      });
+  });
+  res.send(JSON.stringify(boxOffice, null, 4));
 });
+
+// app.get('/', function(req, res){
+//   res.send('Hello Tomato~');
+// });
 
 app.listen(app.get('port'), function() {
   console.log("Tomato Express server is running at localhost:" + app.get('port'));
